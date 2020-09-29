5a46802ba41008101d90f3db12629eb4bd6c242a
Jonathan Cui <jonathancui123@gmail.com>
2020-09-28 22:52:59 -0400

    listen on port

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
+	"net"
 	"os"
+)
+
+var (
+	listenAddr = flag.String("listen", "", "host:port to listen on")
+	dialAddr   = flag.String("dial", "localhost:8000", "host:port to dial")
 )
 
 type Message struct {
@@ -12,8 +20,49 @@ type Message struct {
 }
 
 func main() {
+	flag.Parse()
+
+	go dial(*dialAddr) // start a go-routine for starting connection
+
+	l, err := net.Listen("tcp", *listenAddr) // listen for TCP connections on given address
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go serve(c)
+	}
+
+}
+
+func serve(c net.Conn) {
+	defer c.Close()
+	d := json.NewDecoder(c)
+	for {
+		var m Message
+		err := d.Decode(&m)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("%#v\n", m) // Print in go-syntax representation
+	}
+}
+
+func dial(addr string) {
+
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	lines := bufio.NewScanner(os.Stdin)
-	enc := json.NewEncoder(os.Stdout)
+	enc := json.NewEncoder(conn)
 	for lines.Scan() {
 		message := Message{Body: lines.Text()}
 		err := enc.Encode(message)
@@ -24,5 +73,4 @@ func main() {
 	if err := lines.Err(); err != nil {
 		log.Fatal(err)
 	}
-
 }
