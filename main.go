ec8a802499d2f1cee8a0e562ed59fb1ecd858d6f
Jonathan Cui <jonathancui123@gmail.com>
2020-09-29 13:23:50 -0400

    broadcast to all peers, automatically build peer list

--- main.go
+++ main.go
@@ -8,16 +8,18 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 
 	"github.com/campoy/whispering-gophers/util"
 )
 
 var (
 	self     string
-	dialAddr = flag.String("dial", "localhost:8000", "host:port to dial")
+	peerAddr = flag.String("dial", "", "peer host:port to dial")
 	dialTrue = flag.Bool("dialTrue", true, "determine whether or not to dial a connection")
 )
 
+// Message is a structure for sending messages in this mesh network
 type Message struct {
 	Addr string
 	Body string
@@ -26,18 +28,19 @@ type Message struct {
 func main() {
 	flag.Parse()
 
-	messageCh := make(chan Message)
-
-	if *dialTrue {
-		go dial(*dialAddr, messageCh) // start a go-routine for starting connection
-	}
-
 	l, err := util.Listen() // listen for TCP connections on given address
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("Listening on", l.Addr())
 	self = l.Addr().String()
+
+	if *dialTrue {
+		go dial(*peerAddr) // start a go-routine for starting connection
+	}
+
+	go read() // start a go-routine for reading from stdIn
+
 	for {
 		c, err := l.Accept()
 		if err != nil {
@@ -46,6 +49,58 @@ func main() {
 		go serve(c)
 	}
 
+}
+
+var peers = &Peers{m: make(map[string]chan<- Message)}
+
+// Peers is a structure for concurrent-safe Peers registry
+type Peers struct {
+	m  map[string]chan<- Message
+	mu sync.RWMutex
+}
+
+// Add creates and returns a new channel for a given peer address.
+// If the address already exists in peer registry, return nil
+func (p *Peers) Add(addr string) <-chan Message {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if _, ok := p.m[addr]; ok {
+		return nil
+	}
+	ch := make(chan Message)
+	p.m[addr] = ch
+	return ch
+}
+
+// Remove deletes the specified peer from the registry
+func (p *Peers) Remove(addr string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if _, ok := p.m[addr]; ok {
+		delete(p.m, addr)
+	}
+}
+
+// List returns a slice of all peers in the registry
+func (p *Peers) List() []chan<- Message {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+
+	l := make([]chan<- Message, 0, len(p.m))
+	for _, ch := range p.m {
+		l = append(l, ch)
+	}
+	return l
+}
+
+func broadcast(m Message) {
+	for _, ch := range peers.List() {
+		select {
+		case ch <- m:
+		default: // In a mesh network, its okay to drop messages.
+		}
+	}
 }
 
 func serve(c net.Conn) {
@@ -58,19 +113,19 @@ func serve(c net.Conn) {
 			log.Println(err)
 			return
 		}
+		go dial(m.Addr)
 		fmt.Printf("%#v\n", m) // Print in go-syntax representation
 	}
 }
 
-func read(messageCh chan Message) {
+func read() {
 	lines := bufio.NewScanner(os.Stdin)
-
 	for lines.Scan() {
 		message := Message{
 			Addr: self,
-			Body: lines.Text()}
-
-		messageCh <- message
+			Body: lines.Text(),
+		}
+		broadcast(message)
 
 	}
 	if err := lines.Err(); err != nil {
@@ -78,20 +133,31 @@ func read(messageCh chan Message) {
 	}
 }
 
-func dial(addr string, messageCh chan Message) {
+func dial(addr string) {
+	if addr == self {
+		return // Don't attempt to dial yourself
+	}
+
+	ch := peers.Add(addr)
+	if ch == nil {
+		return // Peer already connected.
+	}
+	defer peers.Remove(addr)
 
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(addr, err)
+		return
 	}
+	defer conn.Close()
 
 	enc := json.NewEncoder(conn)
-	for {
-		err := enc.Encode(<-messageCh)
+	for m := range ch {
+		err := enc.Encode(m)
 
 		if err != nil {
-			log.Fatal(err)
+			log.Println(addr, err)
+			return
 		}
 	}
-
 }
